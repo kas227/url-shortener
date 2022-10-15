7e8c38538f12e3569cab3a71b738c1f72e63c935
Lukas <klamtlukas@gmail.com>
2022-10-15 12:24:38 +0200

    chore: setup expected tests

--- src/test/java/de/klamtluk/urlshortener/api/UrlShortenerControllerTest.java
+++ src/test/java/de/klamtluk/urlshortener/api/UrlShortenerControllerTest.java
@@ -1,12 +1,16 @@
 package de.klamtluk.urlshortener.api;
 
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
+import java.net.URL;
+
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,30 +18,53 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 class UrlShortenerControllerTest {
 
+    static final String URL_PARAM = "url";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnBadRequestWhenUrlMissing() throws Exception {
-        this.mockMvc.perform(get("/"))
-                .andExpect(status().isBadRequest())
-                .andDo(print())
-                .andExpect(content().string(containsString("url")));
+        this.mockMvc.perform(get("/")).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString(URL_PARAM)));
     }
 
     @Test
     public void shouldReturnBadRequestWhenUrlMalformed() throws Exception {
-        this.mockMvc.perform(get("/")
-                        .queryParam("url", "not a url"))
+        this.mockMvc.perform(get("/").queryParam(URL_PARAM, "not a url"))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString("url")));
+                .andExpect(content().string(containsString(URL_PARAM)));
     }
 
     @Test
     public void shouldReturnBadRequestWhenUrlEmpty() throws Exception {
-        this.mockMvc.perform(get("/")
-                        .queryParam("url", ""))
+        this.mockMvc.perform(get("/").queryParam(URL_PARAM, ""))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString("url")));
+                .andExpect(content().string(containsString(URL_PARAM)));
+    }
+
+    @Test
+    public void shouldRedirectedForExistingUrl() throws Exception {
+        final String existingUrl = createNewShortUrl();
+
+        this.mockMvc.perform(get("/").queryParam(URL_PARAM, existingUrl))
+                .andExpect(status().isPermanentRedirect());
+    }
+
+    private String createNewShortUrl() throws Exception {
+        return this.mockMvc.perform(post("/")
+                        .content("""
+                                {"url" : "https://localhost/long-url"}
+                                """))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    public void shouldFailForNotExistingUrl() throws Exception {
+        final String nonExistingUrl = "123";
+        this.mockMvc.perform(get("/").queryParam(URL_PARAM, nonExistingUrl))
+                .andExpect(status().isNotFound());
     }
 }
