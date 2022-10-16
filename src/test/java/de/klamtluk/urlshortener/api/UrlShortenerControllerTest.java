b4d0d51579728c976990d6a96ce567d194a05348
Lukas <klamtlukas@gmail.com>
2022-10-16 08:34:27 +0200

    chore: setup expected tests

--- src/test/java/de/klamtluk/urlshortener/api/UrlShortenerControllerTest.java
+++ src/test/java/de/klamtluk/urlshortener/api/UrlShortenerControllerTest.java
@@ -1,17 +1,13 @@
 package de.klamtluk.urlshortener.api;
 
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.net.URL;
-
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
